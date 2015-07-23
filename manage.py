a7995a895efce8a857c05db7dc9676434511371c
Dimas <dimas.ciputra@gmail.com>
2015-07-23 09:12:07 +0700

    Rename to default

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "something.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "default.settings")
 
     from django.core.management import execute_from_command_line
 
